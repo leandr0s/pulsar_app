e283522d0442acb547a95c7cd5f35407e453e669
leandr0s <leandr0s.fernandes@gmail.com>
2025-04-22 16:46:17 -0300

    ajustes no path

--- controller/precificacaoController.py
+++ controller/precificacaoController.py
@@ -10,23 +10,23 @@ PROJECT_NAME = 'electric-armor-429218-g7'
 
 
 def getPrecificacao(status,limit):
-    df = pd.read_csv('..\\src\\data\\precificacao.csv', sep=';')
+    df = pd.read_csv('../src/data/precificacao.csv', sep=';')
     print(df)
-    df2 = pd.read_csv('..\\src\\data\\log_precificacao.csv', sep=';')
+    df2 = pd.read_csv('../src/data/log_precificacao.csv', sep=';')
     df_final = pd.merge(df,df2, how='left', left_on='codigo', right_on='cod_precificacao')
     print(df_final)
     return df_final
 
 def getDadosPrecificacao():
-    df = pd.read_csv('..\\src\\data\\precificacao.csv', sep=';')
+    df = pd.read_csv('../src/data/precificacao.csv', sep=';')
     return df
 
 def getDadosPrecificacaoSilverByCodigo(cod_precificacao):
-    df = pd.read_csv('..\\src\\data\\precificacao.csv', sep=';')
+    df = pd.read_csv('../src/data/precificacao.csv', sep=';')
     return df
 
 def getDadosPrecificacaoGoldByCodigo(cod_precificacao):
-    df = pd.read_csv('..\\src\\data\\precificacao.csv', sep=';')
+    df = pd.read_csv('../src/data/precificacao.csv', sep=';')
     return df
 
 
